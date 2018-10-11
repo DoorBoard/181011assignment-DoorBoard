3471a09854064285dc8f6a715efd9570482223aa
DoorBoard <43874253+DoorBoard@users.noreply.github.com>
2018-10-12 07:35:50 +0900

    課題を提出します

--- Paiza/paiza-1-08-1.py
+++ Paiza/paiza-1-08-1.py
@@ -14,6 +14,11 @@
 スライムの合計体重はYYキロです
 '''
 import random
+
 number = random.randint(1, 10)  # 匹数 1 ～ 10
+
 print("体重100キロのスライムが" + str(number) + "匹あらわれた")
+
 # 合計体重 = 匹数 x 100
+
+print("スライムの合計体重は" + str(number * 100) + "キロです")
